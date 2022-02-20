00f611e0a8dea69f6bc6e307da90591de010d842
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-02-20 12:15:12 +0100

    Update botMaf.js

--- botMaf.js
+++ botMaf.js
@@ -4,25 +4,31 @@ class BotMaf extends BaseBot {
 
     constructor(){
         super();
-        this.lastMove = 1;
     }
 
     newGeneration(){
         super.newGeneration();
-        this.lastMove = 1;
-    }
-
-    newGame(){
-        this.lastMove = 1;
-    }
-
-    memorize(number){
-        this.lastMove = number;
     }
 
     play(){
-        return this.lastMove;
+        //throw 1;
+        return this.rand50() || this.rand50();
     }
+
+    rand50() {
+        // rand() function will generate odd or even
+        // number with equal probability. If rand()
+        // generates odd number, the function will
+        // return 1 else it will return 0.
+        return Math.floor(Math.random() * 10) & 1;
+    }
+     
+    // Random Function to that returns 1 with 75%
+    // probability and 0 with 25% probability using
+    // Bitwise OR
+    // rand75() {
+    //     return rand50() | rand50();
+    // }
 
     // constructor() {
     //     super();
